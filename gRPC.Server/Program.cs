3473fd45d6eada53bbdbd2a969d99d14923c2f08
Ahmed.Mubarak <Ahmed.Mubarak@AHMED-MUBARAK>
2022-06-20 18:40:42 +0200

    Add project files.

---
+++ gRPC.Server/Program.cs
@@ -0,0 +1,49 @@
+using gRPC.Server.Services;
+using GrpcBrowser.Configuration;
+using ProtoBuf.Grpc.Server;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+
+builder.Services.AddGrpc();
+builder.Services.AddCodeFirstGrpc();
+builder.Services.AddGrpcBrowser();
+
+var app = builder.Build();
+app.UseGrpcBrowser();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //app.UseDeveloperExceptionPage();
+    app.MapGrpcReflectionService();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseRouting();
+
+app.MapGrpcService<FeedService>().AddToGrpcBrowserWithClient<FeedMeMorService.FeedMeMorServiceClient>(); 
+app.UseGrpcBrowser();
+app.MapGrpcBrowser();
+
+app.MapGet("/", context =>
+{
+    context.Response.StatusCode = 302;
+    context.Response.Headers.Add("Location", "https://localhost:7046/grpc");
+    return Task.CompletedTask;
+});
+
+app.Run();
---
+++ gRPC.Server/Services/FeedService.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+
+namespace gRPC.Server.Services
+{
+    public class FeedService : FeedMeMorService.FeedMeMorServiceBase
+    {
+        private readonly ILogger<FeedService> _logger;
+
+        public FeedService(ILogger<FeedService> logger)
+        {
+            this._logger = logger;
+        }
+
+
+        public override Task<HerIsYourFood> AddFood(Food request, ServerCallContext context)
+        {
+            HerIsYourFood food = new HerIsYourFood()
+            {
+                IsServed = "Bon Appetit :) "
+            };
+
+            _logger.LogInformation("Food Served");
+
+            return Task.FromResult(food);
+        }
+    }
+}
